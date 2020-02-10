bb8e14d009b4eaf8d3050d1a04d31a0a78151fcc
Eduardo Ocampo <eduardo.ocampo2728@hotmail.com>
2020-02-10 11:31:29 -0500

    Part 3 incompleto

--- BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -7,9 +7,11 @@ package edu.eci.arsw.blueprints.persistence.impl;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintFilter;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -21,20 +23,20 @@ import java.util.Set;
  *
  * @author hcadavid
  */
+
 @Service
 public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
 
     private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
 
-    //@Autowired
-    //private BlueprintFilter filter;
+    @Autowired
+    private BlueprintFilter filter;
 
     public InMemoryBlueprintPersistence() {
         //load stub data
-        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        Point[] pts = new Point[]{new Point(140, 140),new Point(115, 115)};
+        Blueprint bp = new Blueprint("_authorname_", "_bpname_ ",pts);
         blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
-        
     }    
     
     @Override
---
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/persistence/impl/RedundancyFilter.java
@@ -0,0 +1,19 @@
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintFilter;
+
+import java.util.Collection;
+
+public class RedundancyFilter implements BlueprintFilter {
+
+    @Override
+    public Blueprint filterBlueprint(Blueprint bp) {
+        return null;
+    }
+
+    @Override
+    public Collection<Blueprint> filterBlueprintSet(Collection<Blueprint> bp) {
+        return null;
+    }
+}
